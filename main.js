9c2cc74d5d3927734f1a6049a6847217e3177e8f
Kay Mok <kaymok3@gmail.com>
2018-03-23 11:45:04 -0400

    Add AJAX request to retrieve a pokemon's basic stats and update pokemon font

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+// const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
+const POKEMON_URL = "http://pokeapi.salestock.net/api/v2/pokemon";
+
 $(document).ready(function () {
-	// all my code goes in here
+  $.ajax({
+    url: POKEMON_URL + "/131",
+    type: "GET",
+    success: function(data) {
+      let $pokeNameId = $(`<h1>${data.name}</h1><h1>no: ${data.id}</h1>`);
+      $("#name-display").append($pokeNameId);
+      let $defense = $(`<h3>${data.stats[3].stat.name}: ${data.stats[3].base_stat}</h3>`);
+      let $attack = $(`<h3>${data.stats[4].stat.name}: ${data.stats[4].base_stat}</h3>`)
+      let $hp = $(`<h3>${data.stats[5].stat.name.toUpperCase()}: ${data.stats[5].base_stat}</h3>`)
+      $("#info-display").append($hp).append($attack).append($defense);
+      let abilities = data.abilities;
+      $("#info-display").append("</br><h3>abilities:</h3>");
+      $(abilities).each(function(i) {
+        $("#info-display").append(`<h3>${this.ability.name}</h3>`);
+      })
+    },
+    error: function() {
+      alert("404 not found: lapras");
+    }
+  })
+
 
 });
