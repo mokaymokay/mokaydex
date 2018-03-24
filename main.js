bc7bbc0058de28b843530393863b7b56f2ccd8a1
Kay Mok <kaymok3@gmail.com>
2018-03-24 11:55:12 -0400

    Add Pokemon class

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
-const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
-// const POKEMON_URL = "http://pokeapi.salestock.net/api/v2/pokemon";
+const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/";
+// const POKEMON_URL = "http://pokeapi.salestock.net/api/v2/pokemon"; // backup url
 
-$(document).ready(function () {
+const MOKAYMON = []; // array of pokemon objects
+
+const MOKAYDEX = {
+  all : function() {
+    return MOKAYMON;
+  },
+  get : function(name) {
+
+  }
+};
+
+class Pokemon {
+  constructor(id, name, defense, attack, hp) {
+    this.id = id;
+    this.name = name;
+    this.defense = defense;
+    this.attack = attack;
+    this.hp = hp;
+  }
+}
+
+function setAttributes(id) {
   $.ajax({
-    url: POKEMON_URL + "/131",
+    url: POKEMON_URL + id,
+    dataType: 'json',
     type: "GET",
     success: function(data) {
-      let $pokeNameId = $(`<h2>${data.name}</h2><h2>no: ${data.id}</h2>`);
-      $("#name-display").append($pokeNameId);
-      let $defense = $(`<h3>${data.stats[3].stat.name}: ${data.stats[3].base_stat}</h3>`);
-      let $attack = $(`<h3>${data.stats[4].stat.name}: ${data.stats[4].base_stat}</h3>`)
-      let $hp = $(`<h3>${data.stats[5].stat.name.toUpperCase()}: ${data.stats[5].base_stat}</h3>`)
-      $("#info-display").append($hp).append($attack).append($defense);
-      let abilities = data.abilities;
-      $("#info-display").append("</br><h3>abilities:</h3>");
-      $(abilities).each(function(i) {
-        $("#info-display").append(`<h3>${this.ability.name}</h3>`);
-      })
+      console.log("success!");
+      name = data.name;
+      defenseStat = data.stats[3].base_stat;
+      attackStat = data.stats[4].base_stat;
+      hpStat = data.stats[5].base_stat;
+      let myPokemon = new Pokemon(id, name, defenseStat, attackStat, hpStat);
+      MOKAYMON.push(myPokemon);
     },
     error: function() {
-      alert("404 not found: lapras");
+      alert("404 not found");
     }
   })
+}
 
-});
+setAttributes(130); // gyarados
+setAttributes(131); // lapras
+setAttributes(143); // snorlax
+
+// let $pokeNameId = $(`<h2>${data.name}</h2><h2>no: ${data.id}</h2>`);
+// $("#name-display").append($pokeNameId);
+// let $defense = $(`<h3>${data.stats[3].stat.name}: ${data.stats[3].base_stat}</h3>`);
+// let $attack = $(`<h3>${data.stats[4].stat.name}: ${data.stats[4].base_stat}</h3>`)
+// let $hp = $(`<h3>${data.stats[5].stat.name.toUpperCase()}: ${data.stats[5].base_stat}</h3>`)
+// $("#info-display").append($hp).append($attack).append($defense);
+// let abilities = data.abilities;
+// $("#info-display").append("</br><h3>abilities:</h3>");
+// $(abilities).each(function(i) {
+//   $("#info-display").append(`<h3>${this.ability.name}</h3>`);
+// })
+
+// $(document).ready(function () {
+// })
