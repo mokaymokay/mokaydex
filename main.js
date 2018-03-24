b017b0066880ed4cb8466f681a8707facb6cbb87
Kay Mok <kaymok3@gmail.com>
2018-03-24 19:26:21 -0400

    Add show function to Trainer class to display data

--- main.js
+++ main.js
@@ -16,6 +16,19 @@ class Trainer {
       if (this.pokedex[i].name === pokemon) { return this.pokedex[i] }
     }
   }
+
+  show() {
+    let $pokeNameId = $(`<h2>${this.pokedex[0].name}</h2><h2>no: ${this.pokedex[0].id}</h2>`);
+    $("#name-display").append($pokeNameId);
+    let $defense = $(`<h3>defense: ${this.pokedex[0].defense}</h3>`);
+    let $attack = $(`<h3>attack: ${this.pokedex[0].attack}</h3>`)
+    let $hp = $(`<h3>HP: ${this.pokedex[0].hp}</h3>`)
+    $("#info-display").append($hp).append($attack).append($defense);
+    $("#info-display").append("</br><h3>abilities:</h3>");
+    for (var i = 0; i < this.pokedex[0].abilities.length; i++) {
+      $("#info-display").append(`<h3>${this.pokedex[0].abilities[i]}</h3>`);
+    }
+  }
 }
 
 class Pokemon {
@@ -30,11 +43,11 @@ class Pokemon {
 }
 
 function createPokemon(id) {
-  $.ajax({
+  return $.ajax({
     url: POKEMON_URL + id,
     dataType: 'json',
     type: "GET",
-    success: function(data) {
+  }).done(function(data) {
       console.log("success!");
       name = data.name;
       defenseStat = data.stats[3].base_stat;
@@ -50,30 +63,18 @@ function createPokemon(id) {
       abilities = sortAbilities();
       let myPokemon = new Pokemon(id, name, defenseStat, attackStat, hpStat, abilities);
       newTrainer.pokedex.push(myPokemon);
-    },
-    error: function() {
+  }).fail(function() {
       alert("404 not found");
-    }
-  })
+  });
 }
 
 let newTrainer = new Trainer("mokaymon");
-let squad = [130, 131, 143];
-for (var i = 0; i < squad.length; i++) {
-  createPokemon(squad[i]);
-}
+createPokemon(130).done(function() {
+  newTrainer.show();
+})
 
-// let $pokeNameId = $(`<h2>${data.name}</h2><h2>no: ${data.id}</h2>`);
-// $("#name-display").append($pokeNameId);
-// let $defense = $(`<h3>${data.stats[3].stat.name}: ${data.stats[3].base_stat}</h3>`);
-// let $attack = $(`<h3>${data.stats[4].stat.name}: ${data.stats[4].base_stat}</h3>`)
-// let $hp = $(`<h3>${data.stats[5].stat.name.toUpperCase()}: ${data.stats[5].base_stat}</h3>`)
-// $("#info-display").append($hp).append($attack).append($defense);
-// let abilities = data.abilities;
-// $("#info-display").append("</br><h3>abilities:</h3>");
-// $(abilities).each(function(i) {
-//   $("#info-display").append(`<h3>${this.ability.name}</h3>`);
-// })
+createPokemon(131).done(createPokemon(143));
 
-// $(document).ready(function () {
-// })
+  // $( "#name-display" ).click(function() {
+  //   newTrainer.show();
+  // });
