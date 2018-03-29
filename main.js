710664104f359f219e4a599dc6d47d878c6501eb
Kay Mok <kaymok3@gmail.com>
2018-03-29 13:23:13 -0400

    Display height and weight

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ class Trainer {
   show(i) {
     let $pokeNameId = $(`<h2>${this.pokedex[i].name}</h2><h2>no: ${this.pokedex[i].id}</h2>`);
     $( "#name-display" ).html($pokeNameId);
-    let $baseStats = $(`<h3>HP: ${this.pokedex[i].hp}</h3><div class="stat-bar" style="width: ${this.pokedex[i].hp}px"></div><h3>attack: ${this.pokedex[i].attack}</h3><div class="stat-bar" style="width: ${this.pokedex[i].attack}px"></div><h3>defense: ${this.pokedex[i].defense}</h3><div class="stat-bar" style="width: ${this.pokedex[i].defense}px"></div>`);
+    let $biometrics = $(`<h3>height: ${this.pokedex[i].height / 10}m</h3><h3>weight: ${this.pokedex[i].weight / 10}kg</h3>`);
+    $( "#biometrics" ).html($biometrics);
+    let $baseStats = $(`</br><h3>HP: ${this.pokedex[i].hp}</h3><div class="stat-bar" style="width: ${this.pokedex[i].hp * 2}px"></div><h3>attack: ${this.pokedex[i].attack}</h3><div class="stat-bar" style="width: ${this.pokedex[i].attack * 2}px"></div><h3>defense: ${this.pokedex[i].defense}</h3><div class="stat-bar" style="width: ${this.pokedex[i].defense * 2}px"></div>`);
     $( "#base-stats" ).html($baseStats);
     $( "#abilities" ).html("</br><h3>abilities:</h3>" + `<h3>${this.pokedex[i].abilities.join(", ")}</h3>`);
     let $image = $(`<img class="pokemon-image" src="${this.pokedex[i].image}" alt="${this.pokedex[i].name}">`);
@@ -49,9 +51,11 @@ class Trainer {
 }
 
 class Pokemon {
-  constructor(id, name, defense, attack, hp, abilities, type, image) {
+  constructor(id, name, height, weight, defense, attack, hp, abilities, type, image) {
     this.id = id;
     this.name = name;
+    this.height = height;
+    this.weight = weight;
     this.defense = defense;
     this.attack = attack;
     this.hp = hp;
@@ -70,13 +74,15 @@ function createPokemon(id) {
       console.log("success!");
       let id = data.id;
       let name = data.name;
+      let height = data.height;
+      let weight = data.weight;
       let defenseStat = data.stats[3].base_stat;
       let attackStat = data.stats[4].base_stat;
       let hpStat = data.stats[5].base_stat;
       let abilities = data.abilities.map(x => x.ability.name);
       let type = data.types.map(x => x.type.name);
       let image = data.sprites.front_default;
-      let myPokemon = new Pokemon(id, name, defenseStat, attackStat, hpStat, abilities, type, image);
+      let myPokemon = new Pokemon(id, name, height, weight, defenseStat, attackStat, hpStat, abilities, type, image);
       newTrainer.pokedex.push(myPokemon);
   }).fail(function() {
       if (newTrainer) {
